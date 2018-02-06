156e800a1d3b70e52d057dcfc55f4d5afe680a80
Steph <Steph@itsalocke.com>
2018-02-06 15:21:55 +0000

    More dir tweaks

--- buildpres.sh
+++ buildpres.sh
@@ -2,18 +2,21 @@
 AUTHORNAME="Steph"
 AUTHOREMAIL="Steph@itsalocke.com"
 GITURL="https://$GITHUB_PAT@github.com/$TRAVIS_REPO_SLUG.git"
-DESTURL="https://$GITHUB_PAT@github.com/lockedatapublished/slides.git"
+DESTDIR = "lockedatapublished/slides"
+DESTURL="https://$GITHUB_PAT@github.com/$DESTDIR.git"
 
 git config --global user.name $AUTHORNAME
 git config --global user.email $AUTHOREMAIL
 
 R CMD BATCH './buildpres.R'
-mkdir docs/$TRAVIS_REPO_SLUG
-cp buildpres.Rout docs/$TRAVIS_REPO_SLUG
+LOGDIR="docs/$TRAVIS_REPO_SLUG"
+mkdir -p $LOGDIR
+cp buildpres.Rout $LOGDIR
 
 cd ../..
 git clone $DESTURL
 cp -r $TRAVIS_REPO_SLUG/docs/* slides/static
-git add slides/static
+cd $DESTDIR
+git add static
 git commit -m "Documents produced in clean environment via Travis $TRAVIS_BUILD_NUMBER"
 git push -u  --quiet origin master
